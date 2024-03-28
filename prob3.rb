c2a5641b191a33bb6d5f9d6aa14e5911a34f0cc4
Lakshman-nunna <saibajinunna3@gmail.com>
2024-03-27 22:54:35 -0500

    viwed current time in europe/london

--- prob3.rb
+++ prob3.rb
@@ -5,3 +5,4 @@ username = HTTParty.get('http://worldtimeapi.org/api/timezone/Europe/London')
 time_zone = Time.parse(username.parsed_response['datetime']).strftime('%Y-%m-%d %H:%M:%S')
 
 puts "The current time in Europe/London is #{time_zone}"
+puts ()
